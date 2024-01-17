680bb14de2251b512573bf1ae79c51973ecb7942
OsamaElkot8 <58602694+OsamaElkot8@users.noreply.github.com>
2024-01-17 06:41:15 +0000

    Update time_single_picker.dart
    primaryTextTheme instead of accentTextTheme

--- lib/src/widget/time_single_picker.dart
+++ lib/src/widget/time_single_picker.dart
@@ -1247,19 +1247,19 @@ class _DialState extends State<_Dial> with SingleTickerProviderStateMixin {
           selectedDialValue = widget.selectedTime.hour;
           primaryLabels = _build24HourRing(theme.textTheme, primaryLabelColor);
           secondaryLabels =
-              _build24HourRing(theme.accentTextTheme, secondaryLabelColor);
+              _build24HourRing(theme.primaryTextTheme, secondaryLabelColor);
         } else {
           selectedDialValue = widget.selectedTime.hourOfPeriod;
           primaryLabels = _build12HourRing(theme.textTheme, primaryLabelColor);
           secondaryLabels =
-              _build12HourRing(theme.accentTextTheme, secondaryLabelColor);
+              _build12HourRing(theme.primaryTextTheme, secondaryLabelColor);
         }
         break;
       case _TimePickerMode.minute:
         selectedDialValue = widget.selectedTime.minute;
         primaryLabels = _buildMinutes(theme.textTheme, primaryLabelColor);
         secondaryLabels =
-            _buildMinutes(theme.accentTextTheme, secondaryLabelColor);
+            _buildMinutes(theme.primaryTextTheme, secondaryLabelColor);
         break;
     }
     // return Container(color: Colors.black, child: Text("1111"));
