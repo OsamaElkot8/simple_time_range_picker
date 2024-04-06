3af7a631dc971b79df59b1e754de64de3a7ccb85
OsamaElkot8 <58602694+OsamaElkot8@users.noreply.github.com>
2024-04-06 16:47:56 +0200

    Update time_range_picker.dart

--- lib/src/widget/time_range_picker.dart
+++ lib/src/widget/time_range_picker.dart
@@ -134,7 +134,7 @@ class _TimeRangeDialogState extends State<_TimeRangeDialog>
               : _kTimePickerHeightLandscape,
           child: Scaffold(
             appBar: TabBar(
-                labelColor: Theme.of(context).textTheme.bodyText1!.color,
+                labelColor: Theme.of(context).textTheme.bodySmall!.color,
                 controller: _tabController,
                 tabs: [
                   Tab(
